ce7c75d6ea2827155bfcec332a2ad08afb62f6c1
Banks Malia <banksmalia1@proton.me>
2025-09-25 20:34:17 -0600

    Copy from last branch I completely screwed

--- install.sh
+++ install.sh
@@ -1,189 +1,151 @@
-#! /bin/bash
-clear
+#!/bin/bash
 echo "Hello!"
-echo "This script will automatically install all of the packages needed for this setup and setup their config files."
-echo "Note that this will not do any system-level configuration or setup (e.g. graphics drivers, users, etc.). You should handle these yourself."
+echo "This script will attempt to install the necessary packages and appy all of the configs for these dotfiles."
+echo "You may be prompted for your password."
 sleep 1
-echo ":: Step 1: Information Gathering"
-echo ":: Checking for yay installation..."
 
+echo "Checking for yay installation..."
+sleep 0.5
 if ! command -v yay &> /dev/null
 then
+    echo "Yay is not installed or unuseable in some way."
+    echo "Yay will be installed."
     install_yay=true
-    echo "   - Yay is not installed or is not useable in some form."
 else
+    echo "Yay is installed and useable."
     install_yay=false
-    echo "   - Yay is installed"
 fi
-
-echo "?: Which config version would you like to install?"
-echo "   - 1. Desktop (GPU & weather waybar modules, relaxed idle management)"
-echo "   - 2. Laptop (Battery waybar module, stricter idle management, fingerprint support, touchpad configuration)"
-read -r -p "[1 or 2]=> " config_choice
-if [ "$config_choice" -eq 1 ]; then
-    config_version="desktop"
-    echo ":: The desktop configuration will be installed."
-elif [ "$config_choice" -eq 2 ]; then
-    config_version="laptop"
-    echo ":: The laptop configuration will be installed."
-else
-    echo ":: Invalid choice. Proceeding with desktop version"
-    config_version="desktop"
-fi
-
-echo "?: Would you like to use GNU stow to manage the config files?"
-read -r -p "[y/N]=> " use_stow
-if [[ "$use_stow" =~ ^[Yy]$ ]]; then
-    echo ":: GNU stow will be used to manage config files."
-    use_stow=true
-else
-    echo ":: GNU stow will not be used."
-    use_stow=false
-fi
-
-echo ":: Installation Summary:"
-if [ "$install_yay" = true ]; then
-    echo "   - Yay will be installed."
-else
-    echo "   - Yay will not be installed."
-fi
-
-echo "   - Config version: $config_version"
-
-if [ "$use_stow" = true ]; then
-    echo "   - GNU Stow will be used."
-else
-    echo "   - GNU Stow will not be used."
-fi
-
-echo ":: Make sure everything above is correct. You will not be able to change these during installation."
-echo "?: Would you like to proceed with the installation?"
-read -r -p "[y/N]=> " proceed
-if [[ "$proceed" =~ ^[Yy]$ ]]; then
-    echo ":: Beginning installation..."
-else
-    echo ":: Exiting..."
-    exit 1
-fi
-
-echo ":: Step 2: Installing packages"
-if [ "$install_yay" = true ]; then
-    echo ":: Installing yay..."
-    sudo pacman -S --noconfirm git base-devel
-    git clone "https://aur.archlinux.org/yay.git"
-    cd yay
-    makepkg -si
-    cd ..
-    rm -rf yay
-    echo ":: Yay installed successfully."
-fi
-
-echo -n ":: Compiling package list... "
-package_list=(
-    "git"
-    "hyprland"
-    "kitty"
-    "matugen-bin"
-    "swww"
-    "waybar"
-    "rofi-wayland"
-    "nautilus"
-    "swaync"
-    "hyprlock"
-    "hypridle"
-    "ttf-jetbrains-mono-nerd"
-    "papirus-icon-theme"
-    "cliphist"
-    "adw-gtk-theme"
-    "network-manager-applet"
-    "waypaper"
-    "swayosd"
-    "nwg-look"
-    "xdg-desktop-portal-hyprland"
-    "xdg-desktop-portal-gtk"
-    "hyprpolkitagent"
-    "wlogout"
-    "grimblast"
-    "hyprpicker"
-)
-if [ $use_stow = true ]; then
-    package_list+=("stow")
-fi
-
-if [ $config_version = "laptop" ]; then
-    package_list+=(
-        "brightnessctl"
-        "fprintd"
-    )
-fi
-
-if [ $config_version = "desktop" ]; then
-    package_list+=(
-        "wttrbar"
-    )
-fi
-
-echo "Done"
-
-echo ":: Installing packages with yay..."
-yay -S --needed "${package_list[@]}"
-echo ":: Package installation complete."
 sleep 1
 
-echo ":: Step 3: Cloning git repo"
-cd ~
-git clone https://github.com/bmalia/dotfiles.git
-cd dotfiles
-if [ $config_version = "laptop" ]; then
-    git checkout laptop
-fi
-echo ":: Repo cloned successfully."
+echo "Select the version of the dotfiles you want to install:"
+echo "- Quickshell-based:"
+echo "  - Monet-laptop (1)"
+echo "  - Monet-desktop (Coming soon)"
+echo "- Waybar-based:"
+echo "  - Pixel-laptop (2)"
+echo "  - Pixel-desktop (3)"
+read -p "Enter the number corresponding to your choice: " choice
+case $choice in
+    1)
+        config="monet-laptop";;
+    2)
+        config="pixel-laptop";;
+    3)
+        config="pixel-desktop";;
+    *)
+        echo "Invalid choice. Exiting."
+        exit 1;;
+esac
+echo "Config selected: $config"
+sleep 1
 
-echo ":: Step 4: Configuring dotfiles"
-echo "!: WARNING: This will overwrite any existing config files for hyprland, hyprlock, hypridle, kitty, matugen, rofi, waybar, and wlogout."
-echo ":: You may want to create backups of them before proceeding."
-echo "?: Would you like to proceed?"
-read -r -p "[y/N]=> " proceed
-if [[ "$proceed" =~ ^[Yy]$ ]]; then
-    echo ":: Proceeding with configuration..."
+echo "Would you like to use GNU-Stow to manage the dotfiles?"
+read -p "[Y/n]: " use_stow
+sleep 1
+
+echo "Installation summary:"
+echo "Selected configuration: $config"
+if [[ $install_yay = true ]]; then
+    echo " - Yay will be installed"
+fi
+if [[ $use_stow = y || $use_stow = Y ]]; then
+    echo " - Stow will be used"
 else
-    echo ":: Exiting..."
+    echo " - Dotfiles will be installed manually"
+fi
+
+echo "Would you like to begin the installation?"
+read -p "[y/N]: " confirm
+if [[ $confirm != y && $confirm != Y ]]; then
+    echo "Exiting..."
     exit 1
 fi
 
-echo ":: Deleting current config files..."
-rm -r ~/.config/hypr ~/.config/kitty ~/.config/matugen ~/.config/rofi ~/.config/waybar ~/.config/waypaper ~/.config/wlogout
-
-if [ $use_stow = true ]; then
-    echo ":: Stowing repo to .config..."
-    # stow .
-else
-    echo ":: Copying files to .config..."
-    cp -r .config/* ~/.config/
+sleep 1
+if [[ $install_yay = true ]]; then
+    echo "Phase 0: Installing yay"
+    echo -n "  - Downloading files... "
+    git clone https://aur.archlinux.org/yay.git > /dev/null
+    echo "Done"
+    echo -n"  - Installing package... "
+    cd yay
+    makepkg -si --noconfirm > /dev/null
+    echo "Done"
+    sleep 0.5
+    echo "Yay installed successfully!"
 fi
-echo ":: Config files copied successfully."
 
-echo ":: Step 5: Finalizing setup"
-echo ":: Generating colors with placeholder image..."
-# matugen image ~/dotfiles/scripts/placeholder-wallpaper.png -m dark
-sleep 2
-clear
-echo ":: Installation complete!"
-echo ":: See https://github.com/bmalia/dotfiles/blob/main/INSTALLATION.md#post-installation-stuff for some things you'll need to do after installation."
-echo ":: General use overview:"
-echo "   - Keybinds:"
-echo "       - Open launcher: Super + Space"
-echo "       - Open terminal: Super + T"
-echo "       - Close active window: Super + Q"
-echo "       - Open file manager: Super + E"
-echo "   - A more comprehensive list of keybinds can be found in the README on the GitHub repository (https://github.com/bmalia/dotfiles)."
-echo "   - Waybar:"
-echo "       - Dark/light mode toggle can be found by hovering over the launcher button."
-echo "       - Waybar can occasionally crash when launching a media player or beginning to play something. To restore it, run 'waybar & disown' in a terminal."
-echo "   - Wallpapers:"
-echo "       - Waypaper has been set up to re-generate material colors when switching wallpapers, so just use it to avoid any headaches."
+sleep 1
+echo "Phase 1: Installing packages"
+echo -n "  - Compiling package list for config $config... "
+source packages.conf
+echo "Done"
+echo "  - Installing packages..."
+case $config in
+    monet-laptop)
+        yay -Syu --needed "${base_packages[@]}" "${laptop_packages[@]}" "${monet_packages[@]}";;
+    pixel-laptop)
+        yay -Syu --needed "${base_packages[@]}" "${laptop_packages[@]}" "${legacy_packages[@]}";;
+    pixel-desktop)
+        yay -Syu --needed "${base_packages[@]}" "${legacy_packages[@]}";;
+    *)
+        echo -n "Error"
+        echo "Invalid config value: $config"
+        echo "Exiting..."
+        exit 1;
+esac
+echo "Packages installed successfully!"
+sleep 1
+
+echo "Phase 2: Installing configs"
+echo -n "  - Switching to repo version for $config... "
+cd ~/dotfiles
+git pull > /dev/null
+case $config in
+    monet-laptop)
+        git checkout main > /dev/null;;
+    pixel-laptop)
+        git checkout old-laptop > /dev/null;;
+    pixel-desktop)
+        git checkout old-desktop > /dev/null;;
+    *)
+        echo -n "Error"
+        echo "Invalid config value: $config"
+        echo "Exiting..."
+        exit 1;
+esac
+echo "Done"
 sleep .5
-echo ":: Enjoy your new setup!"
-echo ":: Press any key to reboot the system."
+echo "WARNING: The configs for hyprland/lock/idle, waybar, quickshell, matugen, rofi, kitty, waypaper, and wlogout are about to be removed."
+echo "If you would like to keep these, exit the script and back them up before coming back."
+echo "Would you like to proceed?"
+read -p "[y/N]: " dest_confirm
+if [[ $dest_confirm != y && $dest_confirm != Y ]]; then
+    echo "Exiting..."
+    exit 1
+fi
+sleep 0.5
+echo -n "  - Removing current configs... "
+rm -r ~/.config/hypr ~/.config/kitty ~/.config/matugen ~/.config/quickshell ~/.config/rofi ~/.config/waybar ~/.config/waypaper ~/.config/wlogout > /dev/null
+echo "Done"
+if [[ $use_stow = y || $use_stow = Y ]]; then
+    echo -n "  - Stowing ~/dotfiles to ~/.config... "
+    stow .
+    echo "Done"
+else
+    echo -n "  - Copying ~/dotfiles/.config/ to ~/.config... "
+    cp -r .config/* ~/.config/
+    echo "Done"
+fi
+sleep 0.5
+echo "Configs installed successfully!"
+
+echo "Installation complete!"
+echo "Look at https://github.com/bmalia/dotfiles/blob/main/README.md for keybinds and general help"
+echo "Follow post-installation procedures in https://github.com/bmalia/dotfiles/blob/main/INSTALLATION.md to get everything working properly."
+echo "Open an issue on the GitHub if you notice any problems or have suggestions"
+echo "Enjoy your new desktop!"
+sleep 5
+echo "[Press any key to reboot your system now.]"
 read -n 1 -s
 reboot
