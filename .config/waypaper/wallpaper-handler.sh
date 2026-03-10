6a1ad3490172108f8def204361c0b4f347b23e2e
Banks Malia <banksmalia1@proton.me>
2026-03-10 11:28:55 -0600

    Update for matugen 4

--- .config/waypaper/wallpaper-handler.sh
+++ .config/waypaper/wallpaper-handler.sh
@@ -3,10 +3,25 @@
 wallpaper="$1"
 mode=$(gsettings get org.gnome.desktop.interface color-scheme)
 
-if [ "$mode" = "'prefer-dark'" ]; then
-    matugen image $wallpaper -t scheme-tonal-spot -m dark
+if [ -f "$HOME/.local/state/yosemite/color_index" ]; then
+    color_index=$(cat ~/.local/state/yosemite/color_index)
 else
-    matugen image $wallpaper -t scheme-tonal-spot -m light
+    echo "0" > ~/.local/state/yosemite/color_index
+    color_index=0
 fi
 
-ln -sf "$wallpaper" "$HOME/current_wallpaper"
+if [ -f "$HOME/.local/state/yosemite/scheme" ]; then
+    scheme=$(cat ~/.local/state/yosemite/scheme)
+  else
+    scheme="scheme-tonal-spot"
+    echo "scheme-tonal-spot" > ~/.local/state/yosemite/scheme
+fi
+
+if [ "$mode" = "'prefer-dark'" ]; then
+    matugen image $wallpaper -t "$scheme" -m dark --source-color-index "$color_index"
+else
+    matugen image $wallpaper -t "$scheme" -m light --source-color-index "$color_index"
+fi
+
+# Store wallpaper path in a state file
+echo "$wallpaper" > ~/.local/state/yosemite/wallpaper_path
--- .local/bin/yosemite-resources/yosemite_appearance.sh
+++ .local/bin/yosemite-resources/yosemite_appearance.sh
@@ -16,15 +16,40 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [ "$operation" == "toggle" ]; then
+  # Get wallpaper path from state file
+  if [ -f ~/.local/state/yosemite/wallpaper_path ]; then
+    wallpaper=$(cat ~/.local/state/yosemite/wallpaper_path)
+  else
+    echo "Creating nonexistent wallpaper state file with default wallpaper path."
+    mkdir -p ~/.local/state/yosemite
+    echo "$HOME/dotfiles/assets/default_wallpaper.jpg" > ~/.local/state/yosemite/wallpaper_path
+    wallpaper="$HOME/dotfiles/assets/default_wallpaper.jpg"
+    echo "Created state file. You might have to reset your wallpaper if you want correct colors."
+  fi
+
+  if [ -f "$HOME/.local/state/yosemite/color_index" ]; then
+    color_index=$(cat ~/.local/state/yosemite/color_index)
+  else
+    echo "0" > ~/.local/state/yosemite/color_index
+    color_index=0
+  fi
+
+  if [ -f "$HOME/.local/state/yosemite/scheme" ]; then
+    scheme=$(cat ~/.local/state/yosemite/scheme)
+  else
+    scheme="scheme-tonal-spot"
+    echo "scheme-tonal-spot" > ~/.local/state/yosemite/scheme
+  fi
+
   currentTheme=$(gsettings get org.gnome.desktop.interface color-scheme | tr -d "'")
   echo "Current theme: $currentTheme"
   if [[ "$currentTheme" = "prefer-dark" ]]; then
     gsettings set org.gnome.desktop.interface color-scheme prefer-light
-    matugen image ~/current_wallpaper -t scheme-tonal-spot -m light
+    matugen image "$wallpaper" -t "$scheme" -m light --source-color-index "$color_index"
     echo "Switched to light appearance."
   else
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-    matugen image ~/current_wallpaper -t scheme-tonal-spot -m dark
+    matugen image "$wallpaper" -t "$scheme" -m dark --source-color-index "$color_index"
     echo "Switched to dark appearance."
   fi
 else
